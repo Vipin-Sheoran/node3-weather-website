8dad94fdf75c6c1828553e75504dd6059706d156
vipin sheoran <sheoranvipin2910@gmail.com>
2020-07-15 08:35:48 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,119 @@
+const path=require('path')
+const express=require('express')
+const hbs=require('hbs')
+const { title } = require('process')
+const app=express()
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+//define path for express config
+const publicdir=path.join(__dirname,'../public')   
+const viewspath=path.join(__dirname,'../templates/views')
+const partialspath=path.join(__dirname,'../templates/partials')
+
+//sets handelbars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewspath)
+hbs.registerPartials(partialspath)
+
+//setup static directory to serve
+app.use(express.static(publicdir))      //index.html is a special name
+
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather App',
+        name:'Sheoran Empire'
+    })
+})
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About',
+        name:'Sheoran Empire'
+    })
+})
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        helptext:'this is for helping users',
+        title:'Help Page',
+        name:'Sheoran Empire'
+    }) 
+})
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+      return  res.send({
+          error:'You must provide an address'})
+    }
+
+    geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+        if(error){
+           return res.send({error})
+        }
+       
+            
+        forecast(latitude,longitude,(error,forecastdata)=>{
+            if(error){
+                return res.send({
+                    error
+                })
+            }
+            res.send({
+                forecast:forecastdata,
+                location,
+                address:req.query.address
+            })
+        })
+        })
+    })
+app.get('/products',(req,res)=>{
+    if(!req.query.search){
+        return res.send({
+            error:'write something in search bar'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products:[]
+    })
+})
+app.get('/help/*',(req, res)=>{
+    res.render('404',{
+        title:404,
+        errormessage:'help article not found',
+        name:'vipin sheoran'
+
+    })
+})
+app.get('*',(req, res)=>{
+res.render('404',{
+    title:404,
+    errormessage:'page not found',
+    name:'vipin sheoran'
+})             //* is used to locate files other than which we built
+})
+
+
+
+
+// app.get('/help',(req,res)=>{
+//     res.send([{
+//         name:'vipin',
+//         age:20
+//     }])
+// })
+
+// app.get('/about',(req,res)=>{
+//     res.send('about page')
+// })
+// app.get('/weather',(req,res)=>{
+//     res.send('weather report')
+// })
+
+//app.com
+//app.com/help
+//app.com/about
+
+app.listen(3000,()=>{
+    console.log('server is up on port 3000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require("request")
+
+const forecast=(latitude,longitude,callback)=>{
+    const url='https://api.openweathermap.org/data/2.5/onecall?lat='+latitude+'&lon='+longitude+'&units=metric&appid=d40d5ec821d1ca9db02a80c0066178f3'
+request({url,json:true},(error,{body})=>{
+if(error){
+    callback('unable to connect to server',undefined)
+}
+else if(body.error){
+    callback('please enter valid details',undefined)
+}
+else{
+    callback(undefined,body.daily[0].weather[0].description+' throughout the day. Current temperature is '+body.current.temp+' degrees. Wind speed is '+body.current.wind_speed+' m/s.')
+}
+})
+}
+module.exports=forecast
